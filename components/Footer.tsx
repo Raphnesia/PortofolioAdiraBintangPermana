4388b30af880d795f60206da96dc186b4bf969de
Raphnesia <ryzenaadira@gmail.com>
2024-10-10 17:46:06 +0700

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,32 +10,32 @@ export const Footer = () => {
         "Mengapa Ambil Pendidikan Teknik Informatika?",
         "Bahasa Pemrograman Kesukaan Kamu Apa ?",
         "Bagaimana Plan Kamu Kedepan ?",
-      ];
+    ];
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         console.log(e.target.value);
-      };
+    };
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log("submitted");
-      };
-  return (
-    <div className='py-20 text-center'>
-        <RainbowButton>
-            Mari Kita Belajar Bareng 🔥
-        </RainbowButton>
-        <h1 className='heading font-extrabold text-center justify-center leading-relaxed '> "Masa Depan Adalah Milik Mereka Yang Percaya Dengan Impiannya"
-        </h1>
-        <p className='text-center text-[#a3a3a3] mx-auto max-w-[700px] text-xl/relaxed text-muted-foreground justify-center mt-5 mb-5'>Ada yang ingin disampaikan? 
-        Tulis pesan Anda di bawah, dan mari kita bahas tentang teknologi baru dan mungkin kita bisa mengaplikasikannya hehe. Mohon jangan spam ya guys
-        </p>
-        <PlaceholdersAndVanishInput
-        placeholders={placeholders}
-        onChange={handleChange}
-        onSubmit={onSubmit}
-      />
-    </div>
-    
-  )
+    };
+    return (
+        <div className='py-20 text-center'>
+            <RainbowButton>
+                Mari Kita Belajar Bareng 🔥
+            </RainbowButton>
+            <h1 className='heading font-extrabold text-center justify-center leading-relaxed '>
+                &quot;Masa Depan Adalah Milik Mereka Yang Percaya Dengan Impiannya&quot;
+            </h1>
+            <p className='text-center text-[#a3a3a3] mx-auto max-w-[700px] text-xl/relaxed text-muted-foreground justify-center mt-5 mb-5'>
+                Ada yang ingin disampaikan? Tulis pesan Anda di bawah, dan mari kita bahas tentang teknologi baru dan mungkin kita bisa mengaplikasikannya hehe. Mohon jangan spam ya guys
+            </p>
+            <PlaceholdersAndVanishInput
+                placeholders={placeholders}
+                onChange={handleChange}
+                onSubmit={onSubmit}
+            />
+        </div>
+    )
 }
 
-export default Footer
+export default Footer;
